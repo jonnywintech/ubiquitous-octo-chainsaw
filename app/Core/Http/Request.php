b8b5a9a5914ac9bc358c3cd60ffbbd42a9275553
Nikola Stankovic <jonnywintech@gmail.com>
2024-06-29 00:13:00 +0200

    testing request object

---
+++ app/Core/Http/Request.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Core\Http;
+
+class Request
+{
+    public array $data;
+
+    public function __construct(){
+        $this->data = $this->getBody();
+    }
+    public function getMethod(): string
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    public function getUri(): string
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    public function getBody(): array
+    {
+        $body = [];
+
+        if ($this->getMethod() === 'get') {
+            foreach ($_GET as $key => $value) {
+                $_GET[$key] = filter_input(INPUT_GET, $value, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        if ($this->getMethod() === 'post') {
+            foreach ($_POST as $key => $value) {
+                $_POST[$key] = filter_input(INPUT_POST, $value, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        return $body;
+    }
+}
